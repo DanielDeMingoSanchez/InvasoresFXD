3488dd62eaef0714efb935d4314fdaada2f2574d
DanielDeMingoSanchez <ddemingsan@educacion.navarra.es>
2024-03-28 18:49:06 +0100

    Creamos metodo update para que se muevan.

---
+++ src/main/java/com/aetxabao/invasoresfx/sprite/EnemyShipR.java
@@ -0,0 +1,16 @@
+package com.aetxabao.invasoresfx.sprite;
+
+import com.aetxabao.invasoresfx.util.Rect;
+import javafx.scene.image.Image;
+
+public class EnemyShipR extends EnemyShip {
+
+    public EnemyShipR(Rect gameRect, Image img, int N) {
+        super(gameRect, img, N);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
