0cf2e83330ac2a434e0412702f8ee521927a2c54
DanielDeMingoSanchez <ddemingsan@educacion.navarra.es>
2024-03-28 20:32:13 +0100

    Construyendo nuevo enemigo, con herencia y inferfaz.

--- src/main/java/com/aetxabao/invasoresfx/sprite/EnemyTow.java
+++ src/main/java/com/aetxabao/invasoresfx/sprite/EnemyTow.java
@@ -1,4 +1,22 @@
 package com.aetxabao.invasoresfx.sprite;
 
 public class EnemyTow extends AEnemy implements IHaveShield{
+
+    int impactMax;
+    int impacts;
+    @Override
+    public void update() {
+        updateFrame();
+    }
+
+    public void updateFrame(){
+        int impactsPerFrame = impactMax / (rows - 1); //Nº de impactos en la imagen
+        currentFrame = impacts / impactsPerFrame; //resultado del marco.
+        if (currentFrame >= rows - 1) currentFrame = rows - 2;//comprueba si la imagen se excede o no.
+    }
+
+    @Override
+    public boolean impact() {
+        return false;
+    }
 }
